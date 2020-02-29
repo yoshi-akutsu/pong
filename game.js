199920baa429f1880723220a7567128a42934b15
Yoshi Akutsu <akutsu.yoshi@gmail.com>
2020-02-29 15:54:29 -0500

    Drew blocks and player paddle.

---
+++ game.js
@@ -0,0 +1,38 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+function newGame() {
+    // Set blank canvas
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    c.fillRect(0,0, canvas.width, canvas.height);
+
+    makeBlocks();
+    makePlayer();
+    
+}
+
+function makeBlocks() {
+    let blockWidth = window.innerWidth / 10;
+    // Make rows
+    for (let j = 0; j < 4; j++) {
+        // Fill rows
+        for (let i = 0; i < 10; i++) {
+            c.beginPath();
+            c.strokeStyle = 'white';
+            c.rect(blockWidth * i, j * 50, blockWidth, 50);
+            c.stroke();
+        }
+    }  
+}
+
+function makePlayer() {
+    console.log('made')
+    let playerWidth = window.innerWidth / 8;
+    c.beginPath();
+    c.strokeStyle = 'white';
+    c.rect(window.innerWidth / 2 - (playerWidth / 2), window.innerHeight / 1.1, playerWidth, 10);
+    c.stroke(); 
+}
+
+newGame();
