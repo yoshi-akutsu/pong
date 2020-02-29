bc1efb266c65fa2ed1248eccd916bf658f78ee2f
Yoshi Akutsu <akutsu.yoshi@gmail.com>
2020-02-29 15:58:55 -0500

    Complete initial ball drawing function.

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ function newGame() {
 
     makeBlocks();
     makePlayer();
+    makeBall();
     
 }
 
@@ -22,17 +23,27 @@ function makeBlocks() {
             c.strokeStyle = 'white';
             c.rect(blockWidth * i, j * 50, blockWidth, 50);
             c.stroke();
+            c.closePath();
         }
     }  
 }
 
 function makePlayer() {
-    console.log('made')
     let playerWidth = window.innerWidth / 8;
     c.beginPath();
     c.strokeStyle = 'white';
     c.rect(window.innerWidth / 2 - (playerWidth / 2), window.innerHeight / 1.1, playerWidth, 10);
     c.stroke(); 
+    c.closePath();
+}
+
+function makeBall() {
+    let playerWidth = window.innerWidth / 8;
+    c.beginPath();
+    c.strokeStyle = 'white';
+    c.arc(window.innerWidth / 2, window.innerHeight / 1.1 - 10, 10, 0, 2 * Math.PI);
+    c.stroke();
+    c.closePath();
 }
 
 newGame();
